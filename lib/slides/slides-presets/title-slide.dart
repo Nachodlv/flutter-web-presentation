19ef82c6c36d30ae62f85bee7eadac0632b17b73
Ignacio de la Vega <ignacio.delavega@ing.austral.edu.ar>
2020-06-10 15:44:21 -0300

    Add links to the starting slide

--- lib/slides/slides-presets/title-slide.dart
+++ lib/slides/slides-presets/title-slide.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class TitleSlide extends StatelessWidget {
   @override
@@ -17,12 +18,22 @@ class TitleSlide extends StatelessWidget {
             Positioned(
                 left: 30,
                 top: 250,
-                child: Text(
-                  "Flutter Web",
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 100,
-                      fontWeight: FontWeight.bold),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 20),
+                      child: Text(
+                        "Flutter Web",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 100,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    _linkText('https://github.com/Nachodlv/flutter-web-presentation'),
+                    _linkText('https://nachodlv.github.io/flutter-web-presentation'),
+                  ],
                 )),
             Positioned(
                 left: 30,
@@ -36,4 +47,14 @@ class TitleSlide extends StatelessWidget {
       ),
     );
   }
+  
+  Widget _linkText(String text) =>
+      Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        child: InkWell(
+          onTap: () => launch(text),
+            child: Text(text, style: TextStyle(fontSize: 30, color: Colors.blue),)
+        ),
+      );
+    
 }
